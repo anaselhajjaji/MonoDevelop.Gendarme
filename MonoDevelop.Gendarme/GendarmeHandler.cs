67ecf8f43579ac35466b8b7b77d59cdb1bc58b38
anaselhajjaji <elhajjaji.anas@gmail.com>
2015-01-15 00:01:28 +0000

    Some improvements.

--- MonoDevelop.Gendarme/GendarmeHandler.cs
+++ MonoDevelop.Gendarme/GendarmeHandler.cs
@@ -31,12 +31,28 @@ namespace MonoDevelop.Gendarme
         {
             if (currentGendarmeAnalysisOperation != null && !currentGendarmeAnalysisOperation.IsCompleted)
                 return;
-                             
+
+            ConfigurationSelector configuration = null;
+
+            if (!(IdeApp.ProjectOperations.CurrentSelectedItem is Project) && !(IdeApp.ProjectOperations.CurrentSelectedItem is Solution)) {
+                return;
+            }
+            
+            // IS PROJECT ?
+            if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
+                configuration = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).DefaultConfiguration.Selector;
+            }
+
+            // IS SOLUTION ?
+            if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
+                configuration = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).DefaultConfiguration.Selector;
+            }
+
             IProgressMonitor monitor = IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor ();
             currentGendarmeAnalysisOperation = monitor.AsyncOperation;
             DispatchService.BackgroundDispatch (new StatefulMessageHandler (BuildAnalyzeAsync), new object[] {
                 monitor,
-                IdeApp.ProjectOperations.CurrentSelectedProject.DefaultConfiguration.Selector
+                configuration
             });
         }
 
@@ -47,14 +63,32 @@ namespace MonoDevelop.Gendarme
             ConfigurationSelector configuration = (ConfigurationSelector)data [1];
                          
             try {
-                BuildResult result = IdeApp.ProjectOperations.CurrentSelectedProject.Build (monitor, 
-                                         configuration);
+                BuildResult result = null;
 
-                if (!result.Failed) {
-                    List<FilePath> files = IdeApp.ProjectOperations.CurrentSelectedProject
-                    .GetOutputFiles (IdeApp.ProjectOperations.CurrentSelectedProject.DefaultConfiguration.Selector);
-                    Analyze (files);
-                    
+                if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
+                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).Build (monitor, 
+                        configuration);
+                }
+                else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
+                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).Build (monitor, 
+                        configuration);
+                }
+
+                if (result != null && !result.Failed) {
+                    List<FilePath> files = null;
+
+                    if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
+                        files = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).GetOutputFiles(configuration);
+                    }
+                    else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
+                        files = new List<FilePath>();
+                        ReadOnlyCollection<Project> projects = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).GetAllProjects();
+                        foreach (Project proj in projects) {
+                            files.AddRange(proj.GetOutputFiles(configuration));
+                        }
+                    }
+
+                    Analyze (monitor, files);
                 }
             } catch (Exception ex) {
                 monitor.ReportError (GettextCatalog.GetString ("MonoGendarme Analyze failed."), ex);
@@ -67,14 +101,15 @@ namespace MonoDevelop.Gendarme
 
         protected override void Update (CommandInfo info)
         {
-            Document doc = IdeApp.Workbench.ActiveDocument;  
-            info.Enabled = doc != null && doc.GetContent<ITextEditorDataProvider> () != null;  
+            if ((IdeApp.ProjectOperations.CurrentSelectedItem is Project)
+                || (IdeApp.ProjectOperations.CurrentSelectedItem is Solution)) {
+                info.Enabled = true;
+            }
         }
 
-        private void Analyze (List<FilePath> files)
+        private void Analyze (IProgressMonitor monitor, List<FilePath> files)
         {
             try {
-
                 GendarmeRunner aRunner = new GendarmeRunner ();
                 aRunner.LoadRules ();
                 aRunner.Reset ();
@@ -82,62 +117,48 @@ namespace MonoDevelop.Gendarme
 
                 foreach (FilePath file in files) {
                     try {
-                        aRunner.Assemblies.Add (AssemblyDefinition.ReadAssembly (file.FullPath, new ReaderParameters { AssemblyResolver = AssemblyResolver.Resolver }));
-                    } catch (BadImageFormatException) {
-
+                        if (!file.Extension.Equals(".mdb")) {
+                            aRunner.Assemblies.Add (AssemblyDefinition.ReadAssembly (file.FullPath, new ReaderParameters { AssemblyResolver = AssemblyResolver.Resolver }));
+                        }
+                    } catch (BadImageFormatException bife) {
+                        monitor.ReportError("Can't add assembly " + file.FullPath, bife); 
                     } catch (FileNotFoundException fnfe) {
-
-                    } finally {
-
+                        monitor.ReportError("Can't add assembly " + file.FullPath, fnfe);
                     }
                 }
 
                 aRunner.Execute ();
 
-                // Clear all the errors if there is any.
-                TaskService.Errors.ClearByOwner(this);
-
-                List<Task> gendarmeAnalysisResultList = new List<Task> ();
-
-                Collection<Defect> defects = aRunner.Defects;
-                foreach (Defect defect in defects) {
-                    
-                    string filePath = string.Empty;
-                    int lineNumber = 0;
-
-                    if (!string.IsNullOrEmpty (defect.Source)) {
-                        filePath = defect.Source.Substring (0, defect.Source.IndexOf ("("));
-
-                        // Get line number
-                        int starts = defect.Source.IndexOf ("(") + 2;
-                        string lineNumberStr = defect.Source.Substring (starts,
-                                               defect.Source.IndexOf (")") - starts);
-                        lineNumber = Convert.ToInt32 (lineNumberStr);
-                    }
-
-                    // warning description
-                    string warningDesc = defect.Rule.Name + ": " + defect.Rule.Problem
-                        + " The solution: " + defect.Rule.Solution;
-
-                    Task gendarmeWarning = new Task (new FilePath (filePath),
-                        warningDesc,
-                        0,
-                        lineNumber,
-                        TaskSeverity.Warning,
-                        TaskPriority.Normal,
-                        IdeApp.ProjectOperations.CurrentSelectedProject,
-                        this);
-
-                    gendarmeAnalysisResultList.Add (gendarmeWarning);
-
-                }
-                TaskService.Errors.AddRange (gendarmeAnalysisResultList);
-                TaskService.Errors.ResetLocationList ();
-                IdeApp.Workbench.ActiveLocationList = TaskService.Errors;
-
+                ReportDefects (aRunner);
             } catch (Exception e) {
-
+                monitor.ReportError("Failed to analyse.", e);
             }
+        }
+
+        void ReportDefects (GendarmeRunner aRunner)
+        {
+            // Clear all the errors if there is any.
+            TaskService.Errors.ClearByOwner (this);
+            List<Task> gendarmeAnalysisResultList = new List<Task> ();
+            Collection<Defect> defects = aRunner.Defects;
+            foreach (Defect defect in defects) {
+                string filePath = string.Empty;
+                int lineNumber = 0;
+                if (!string.IsNullOrEmpty (defect.Source)) {
+                    filePath = defect.Source.Substring (0, defect.Source.IndexOf ("("));
+                    // Get line number
+                    int starts = defect.Source.IndexOf ("(") + 2;
+                    string lineNumberStr = defect.Source.Substring (starts, defect.Source.IndexOf (")") - starts);
+                    lineNumber = Convert.ToInt32 (lineNumberStr);
+                }
+                // warning description
+                string warningDesc = defect.Rule.Name + ": " + defect.Rule.Problem + " The solution: " + defect.Rule.Solution;
+                Task gendarmeWarning = new Task (new FilePath (filePath), warningDesc, 0, lineNumber, TaskSeverity.Warning, TaskPriority.Normal, IdeApp.ProjectOperations.CurrentSelectedProject, this);
+                gendarmeAnalysisResultList.Add (gendarmeWarning);
+            }
+            TaskService.Errors.AddRange (gendarmeAnalysisResultList);
+            TaskService.Errors.ResetLocationList ();
+            IdeApp.Workbench.ActiveLocationList = TaskService.Errors;
         }
     }
 }
