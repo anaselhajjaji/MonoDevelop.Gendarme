ca5c49b3bfa40e86abab593bd7875e8e694beda1
anaselhajjaji <elhajjaji.anas@gmail.com>
2015-11-21 11:22:30 +0000

    Format code.

--- MonoDevelop.Gendarme/GendarmeHandler.cs
+++ MonoDevelop.Gendarme/GendarmeHandler.cs
@@ -27,143 +27,172 @@ namespace MonoDevelop.Gendarme
     {
         static IAsyncOperation currentGendarmeAnalysisOperation = MonoDevelop.Core.ProgressMonitoring.NullAsyncOperation.Success;
 
-        protected override void Run ()
+        protected override void Run()
         {
             if (currentGendarmeAnalysisOperation != null && !currentGendarmeAnalysisOperation.IsCompleted)
                 return;
 
             ConfigurationSelector configuration = null;
 
-            if (!(IdeApp.ProjectOperations.CurrentSelectedItem is Project) && !(IdeApp.ProjectOperations.CurrentSelectedItem is Solution)) {
+            if (!(IdeApp.ProjectOperations.CurrentSelectedItem is Project) && !(IdeApp.ProjectOperations.CurrentSelectedItem is Solution))
+            {
                 return;
             }
             
             // IS PROJECT ?
-            if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
+            if (IdeApp.ProjectOperations.CurrentSelectedItem is Project)
+            {
                 configuration = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).DefaultConfiguration.Selector;
             }
 
             // IS SOLUTION ?
-            if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
+            if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution)
+            {
                 configuration = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).DefaultConfiguration.Selector;
             }
 
-            IProgressMonitor monitor = IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor ();
+            IProgressMonitor monitor = IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor();
             currentGendarmeAnalysisOperation = monitor.AsyncOperation;
-            DispatchService.BackgroundDispatch (new StatefulMessageHandler (BuildAnalyzeAsync), new object[] {
-                monitor,
-                configuration
-            });
+            DispatchService.BackgroundDispatch(new StatefulMessageHandler(BuildAnalyzeAsync), new object[]
+                {
+                    monitor,
+                    configuration
+                });
         }
 
-        void BuildAnalyzeAsync (object ob)
+        void BuildAnalyzeAsync(object ob)
         {
             object[] data = (object[])ob;
-            IProgressMonitor monitor = (IProgressMonitor)data [0];
-            ConfigurationSelector configuration = (ConfigurationSelector)data [1];
+            IProgressMonitor monitor = (IProgressMonitor)data[0];
+            ConfigurationSelector configuration = (ConfigurationSelector)data[1];
                          
-            try {
+            try
+            {
                 BuildResult result = null;
 
-                if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
-                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).Build (monitor, 
+                if (IdeApp.ProjectOperations.CurrentSelectedItem is Project)
+                {
+                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).Build(monitor, 
                         configuration);
                 }
-                else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
-                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).Build (monitor, 
+                else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution)
+                {
+                    result = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).Build(monitor, 
                         configuration);
                 }
 
-                if (result != null && !result.Failed) {
+                if (result != null && !result.Failed)
+                {
                     List<FilePath> files = null;
 
-                    if (IdeApp.ProjectOperations.CurrentSelectedItem is Project) {
+                    if (IdeApp.ProjectOperations.CurrentSelectedItem is Project)
+                    {
                         files = (IdeApp.ProjectOperations.CurrentSelectedItem as Project).GetOutputFiles(configuration);
                     }
-                    else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution) {
+                    else if (IdeApp.ProjectOperations.CurrentSelectedItem is Solution)
+                    {
                         files = new List<FilePath>();
                         ReadOnlyCollection<Project> projects = (IdeApp.ProjectOperations.CurrentSelectedItem as Solution).GetAllProjects();
-                        foreach (Project proj in projects) {
+                        foreach (Project proj in projects)
+                        {
                             files.AddRange(proj.GetOutputFiles(configuration));
                         }
                     }
 
-                    Analyze (monitor, files);
+                    Analyze(monitor, files);
                 }
-            } catch (Exception ex) {
-                monitor.ReportError (GettextCatalog.GetString ("MonoGendarme Analyze failed."), ex);
-            } finally {
-                monitor.Log.WriteLine ();
-                monitor.Log.WriteLine (GettextCatalog.GetString ("MonoGendarme Analyze Done."));
-                monitor.Dispose ();
+            }
+            catch (Exception ex)
+            {
+                monitor.ReportError(GettextCatalog.GetString("MonoGendarme Analyze failed."), ex);
+            }
+            finally
+            {
+                monitor.Log.WriteLine();
+                monitor.Log.WriteLine(GettextCatalog.GetString("MonoGendarme Analyze Done."));
+                monitor.Dispose();
             }
         }
 
-        protected override void Update (CommandInfo info)
+        protected override void Update(CommandInfo info)
         {
             if ((IdeApp.ProjectOperations.CurrentSelectedItem is Project)
-                || (IdeApp.ProjectOperations.CurrentSelectedItem is Solution)) {
+                || (IdeApp.ProjectOperations.CurrentSelectedItem is Solution))
+            {
                 info.Enabled = true;
             }
         }
 
-        private void Analyze (IProgressMonitor monitor, List<FilePath> files)
+        private void Analyze(IProgressMonitor monitor, List<FilePath> files)
         {
-            try {
-                GendarmeRunner aRunner = new GendarmeRunner ();
-                aRunner.LoadRules ();
-                aRunner.Reset ();
-                aRunner.Assemblies.Clear ();
+            try
+            {
+                GendarmeRunner aRunner = new GendarmeRunner();
+                aRunner.LoadRules();
+                aRunner.Reset();
+                aRunner.Assemblies.Clear();
 
-                foreach (FilePath file in files) {
-                    try {
-                        if (file.Extension.Equals(".dll") || file.Extension.Equals(".exe")) {
-                            monitor.Log.WriteLine (GettextCatalog.GetString ("Register assembly " + file.FullPath + " for analysis."));
-                            aRunner.Assemblies.Add (AssemblyDefinition.ReadAssembly (file.FullPath, new ReaderParameters { AssemblyResolver = AssemblyResolver.Resolver }));
+                foreach (FilePath file in files)
+                {
+                    try
+                    {
+                        if (file.Extension.Equals(".dll") || file.Extension.Equals(".exe"))
+                        {
+                            monitor.Log.WriteLine(GettextCatalog.GetString("Register assembly " + file.FullPath + " for analysis."));
+                            aRunner.Assemblies.Add(AssemblyDefinition.ReadAssembly(file.FullPath, new ReaderParameters { AssemblyResolver = AssemblyResolver.Resolver }));
                         }
-                        else {
-                            monitor.Log.WriteLine (GettextCatalog.GetString ("Skipping the file " + file.FullPath + "."));
+                        else
+                        {
+                            monitor.Log.WriteLine(GettextCatalog.GetString("Skipping the file " + file.FullPath + "."));
                         }
-                    } catch (BadImageFormatException bife) {
+                    }
+                    catch (BadImageFormatException bife)
+                    {
                         monitor.ReportError("Can't add assembly " + file.FullPath, bife); 
-                    } catch (FileNotFoundException fnfe) {
+                    }
+                    catch (FileNotFoundException fnfe)
+                    {
                         monitor.ReportError("Can't add assembly " + file.FullPath, fnfe);
                     }
                 }
 
-                aRunner.Execute ();
+                aRunner.Execute();
 
-                ReportDefects (aRunner);
-            } catch (Exception e) {
+                ReportDefects(aRunner);
+            }
+            catch (Exception e)
+            {
                 monitor.ReportError("Failed to analyse.", e);
             }
         }
 
-        void ReportDefects (GendarmeRunner aRunner)
+        void ReportDefects(GendarmeRunner aRunner)
         {
             // Clear all the errors if there is any.
-            TaskService.Errors.ClearByOwner (this);
-            List<Task> gendarmeAnalysisResultList = new List<Task> ();
+            TaskService.Errors.ClearByOwner(this);
+            List<Task> gendarmeAnalysisResultList = new List<Task>();
             Collection<Defect> defects = aRunner.Defects;
-            foreach (Defect defect in defects) {
+            foreach (Defect defect in defects)
+            {
                 string filePath = string.Empty;
                 int lineNumber = 0;
-                if (!string.IsNullOrEmpty (defect.Source)) {
-                    filePath = defect.Source.Substring (0, defect.Source.IndexOf ("("));
+                if (!string.IsNullOrEmpty(defect.Source))
+                {
+                    filePath = defect.Source.Substring(0, defect.Source.IndexOf("("));
                     // Get line number
-                    int starts = defect.Source.IndexOf ("(") + 2;
-                    string lineNumberStr = defect.Source.Substring (starts, defect.Source.IndexOf (")") - starts);
-                    lineNumber = Convert.ToInt32 (lineNumberStr);
+                    int starts = defect.Source.IndexOf("(") + 2;
+                    string lineNumberStr = defect.Source.Substring(starts, defect.Source.IndexOf(")") - starts);
+                    lineNumber = Convert.ToInt32(lineNumberStr);
                 }
                 // warning description
-                string warningDesc = defect.Rule.Name + ": " + defect.Rule.Problem 
-                    + " The solution: " + defect.Rule.Solution
-                    + "\r\nMore information: " + defect.Rule.Uri.ToString();
-                Task gendarmeWarning = new Task (new FilePath (filePath), warningDesc, 0, lineNumber, TaskSeverity.Warning, TaskPriority.Normal, IdeApp.ProjectOperations.CurrentSelectedProject, this);
-                gendarmeAnalysisResultList.Add (gendarmeWarning);
+                string warningDesc = defect.Rule.Name + ": " + defect.Rule.Problem
+                                     + " The solution: " + defect.Rule.Solution
+                                     + "\r\nMore information: " + defect.Rule.Uri.ToString();
+                Task gendarmeWarning = new Task(new FilePath(filePath), warningDesc, 0, lineNumber, TaskSeverity.Warning, TaskPriority.Normal, IdeApp.ProjectOperations.CurrentSelectedProject, this);
+                gendarmeAnalysisResultList.Add(gendarmeWarning);
             }
-            TaskService.Errors.AddRange (gendarmeAnalysisResultList);
-            TaskService.Errors.ResetLocationList ();
+            TaskService.Errors.AddRange(gendarmeAnalysisResultList);
+            TaskService.Errors.ResetLocationList();
             IdeApp.Workbench.ActiveLocationList = TaskService.Errors;
         }
     }
--- MonoDevelop.Gendarme/GendarmeRunner.cs
+++ MonoDevelop.Gendarme/GendarmeRunner.cs
@@ -7,60 +7,66 @@ using Gendarme.Framework.Engines;
 
 namespace MonoDevelop.Gendarme
 {
-    [EngineDependency (typeof (SuppressMessageEngine))]
+    [EngineDependency(typeof(SuppressMessageEngine))]
     public class GendarmeRunner : Runner
     {
-        private static TypeFilter RuleTypeFilter = new TypeFilter (RuleFilter);
+        private static TypeFilter RuleTypeFilter = new TypeFilter(RuleFilter);
 
-        public GendarmeRunner ()
+        public GendarmeRunner()
         {
-            IgnoreList = new BasicIgnoreList (this);
+            IgnoreList = new BasicIgnoreList(this);
         }
 
-        private static bool RuleFilter (Type type, object interfaceName)
+        private static bool RuleFilter(Type type, object interfaceName)
         {
-            return (type.ToString () == (interfaceName as string));
+            return (type.ToString() == (interfaceName as string));
         }
 
-        private void LoadRulesFromAssembly (string assemblyName)
+        private void LoadRulesFromAssembly(string assemblyName)
         {
-            AssemblyName aname = AssemblyName.GetAssemblyName (Path.GetFullPath (assemblyName));
-            Assembly a = Assembly.Load (aname);
-            foreach (Type t in a.GetTypes ()) {
+            AssemblyName aname = AssemblyName.GetAssemblyName(Path.GetFullPath(assemblyName));
+            Assembly a = Assembly.Load(aname);
+            foreach (Type t in a.GetTypes ())
+            {
                 if (t.IsAbstract || t.IsInterface)
                     continue;
-                if (t.FindInterfaces (RuleTypeFilter, "Gendarme.Framework.IRule").Length > 0) {
-                    Rules.Add ((IRule) Activator.CreateInstance (t));
+                if (t.FindInterfaces(RuleTypeFilter, "Gendarme.Framework.IRule").Length > 0)
+                {
+                    Rules.Add((IRule)Activator.CreateInstance(t));
                 }
             }
         }
 
-        public void LoadRules ()
+        public void LoadRules()
         {
             // load every dll to check for rules...
-            string dir = Path.GetDirectoryName (typeof (IRule).Assembly.Location);
-            FileInfo [] files = new DirectoryInfo (dir).GetFiles ("*.dll");
-            foreach (FileInfo info in files) {
+            string dir = Path.GetDirectoryName(typeof(IRule).Assembly.Location);
+            FileInfo[] files = new DirectoryInfo(dir).GetFiles("*.dll");
+            foreach (FileInfo info in files)
+            {
                 // except for a few, well known, ones
-                switch (info.Name) {
-                case "Mono.Cecil.dll":
-                case "Mono.Cecil.Pdb.dll":
-                case "Mono.Cecil.Mdb.dll":
-                case "Gendarme.Framework.dll":
-                    continue;
+                switch (info.Name)
+                {
+                    case "Mono.Cecil.dll":
+                    case "Mono.Cecil.Pdb.dll":
+                    case "Mono.Cecil.Mdb.dll":
+                    case "Gendarme.Framework.dll":
+                        continue;
                 }
-                LoadRulesFromAssembly (info.FullName);
+                LoadRulesFromAssembly(info.FullName);
             }
         }
 
-        public void Execute ()
+        public void Execute()
         {
-            try {
-                Initialize ();
-                Run ();
-                TearDown ();
+            try
+            {
+                Initialize();
+                Run();
+                TearDown();
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
             }
         }
     }
