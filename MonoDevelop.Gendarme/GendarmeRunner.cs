608362e1d0be7f72bea571df3aeff3e972be7763
anaselhajjaji <elhajjaji.anas@gmail.com>
2015-12-11 11:42:54 +0000

    Add class header

--- MonoDevelop.Gendarme/GendarmeRunner.cs
+++ MonoDevelop.Gendarme/GendarmeRunner.cs
@@ -1,4 +1,24 @@
-﻿using Gendarme.Framework;
+﻿//-----------------------------------------------------------------------
+// <copyright file="GendarmeRunner.cs">
+//   APL 2.0
+// </copyright>
+// <license>
+//   Copyright 2015 Anas EL HAJJAJI
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </license>
+//-----------------------------------------------------------------------
+using Gendarme.Framework;
 using Gendarme.Framework.Engines;
 
 namespace MonoDevelop.Gendarme
